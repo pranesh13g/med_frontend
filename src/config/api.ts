427ef608354ed9cea7e4136afee868eda50b6354
pranesh13g <praneshck8@gmail.com>
2025-11-13 16:20:49 +0600

    server changed

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,9 @@
 // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
- const API_URL = import.meta.env.VITE_API_URL || 'https://med-backend-sigma.vercel.app/api';
+//  const API_URL =  'https://med-backend-sigma.vercel.app/api';
+const API_URL =  'https://med-backend-b0e1.onrender.com/api';
+
+
 
 export const api = {
   async request(endpoint: string, options: RequestInit = {}) {
